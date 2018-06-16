28caf8ed263ee253b72acb2eec054ca2971ac903
takattata <ticktack016@gmail.com>
2018-06-16 10:44:12 +0900

    [modify] Create looks

--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -1,12 +1,44 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
+  final List<String> _words = ["aaa", "行なう人だけが成功する"];
+  final List<Image> _imagesOfBg = [
+    Image.asset(
+      'assets/images/image_bg00.png',
+      fit: BoxFit.fill,
+    ),
+    Image.asset(
+      'assets/images/image_bg01.png',
+      fit: BoxFit.fill,
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
+    const double sideMargin = 32.0;
     return new Scaffold(
-      body: new Container(
-        color: Colors.yellow,
-      ),
+      body: Stack(
+        children: <Widget>[
+          SizedBox.expand(
+              child: _imagesOfBg[Random().nextInt(_imagesOfBg.length)]
+          ),
+          Center(
+            child: Container(
+              margin: const EdgeInsets.only(
+                left: sideMargin, right: sideMargin
+              ),
+              child: Text(
+                _words[1],
+                style: TextStyle(
+                  fontSize: 26.0,
+                ),
+              ),
+            )
+          ),
+        ],
+      )
     );
   }
 }
